c8883cfe465fa9c570e495767656489bc2d2f73a
Tan Nguyen <tan.nguyen2@halliburton.com>
2020-06-23 16:46:37 -0500

    read data code

---
+++ plot.R
@@ -0,0 +1,11 @@
+library(dplyr)
+library(lubridate)
+
+setAs("character","myDate", function(from) dmy(from) )
+setAs("character","myTime", function(from) hms(from) )
+classes <- c("myDate", "myTime", rep("numeric", 7))
+
+data <- read.csv("household_power_consumption.txt", na.strings = "?", sep = ";", comment.char = "",
+                 colClasses = classes)
+data <- tibble(data)
+data <- filter(data, Date == ymd("2007-02-01") | Date == ymd("2007-02-02"))
