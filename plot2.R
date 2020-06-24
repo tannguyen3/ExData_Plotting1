ae27f7021b30e244e14d0771c877173fb7ec4e7a
Tan Nguyen <tan.nguyen2@halliburton.com>
2020-06-23 22:29:24 -0500

    finish plot2

--- plot2.R
+++ plot2.R
@@ -1,4 +1,9 @@
 source("prepareData.R")
 
 plotFile <- file.path(plotsFolder, "plot2.png")
-with(data, plot(global_active_power, type = "l", ylab = "Global Active Power (kilowatts)"))
+png(filename = plotFile, width = 480, height = 480)
+
+data2 <- mutate(data, datetime = date + time )
+with(data2, plot(datetime, global_active_power, type = "l", ylab = "Global Active Power (kilowatts)", xlab = ""))
+
+dev.off()
